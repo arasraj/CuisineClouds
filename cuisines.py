9bd2ccf242038d03f4c80935301dc0648f56f8da
Raj Arasu <arasraj@gmail.com>
2011-08-09 17:00:56 -0400

    added sqlite support

--- cuisines.py
+++ cuisines.py
@@ -4,6 +4,7 @@ import urllib
 import BeautifulSoup
 import operator
 import stemmer
+import sqlite3
 import sys
 from urllib2 import *
 import cPickle as pickle
@@ -13,12 +14,13 @@ URL_BASE = 'http://www.foodnetwork.com'
 
 def retrieve_cuisine():
   url = URL_BASE + '/topics/%s/index.html?No=%d'
-  cuisines = ['indian']
+  cuisines = ['mexican', 'indian']
 
-  alllinks = []
+  cuisine_dict = {}
   for cuisine in cuisines:
-    #for i in range(0, 20, 20):
-    for i in range(0, 100, 20):
+    alllinks = []
+    for i in range(0, 20, 20):
+    #for i in range(0, 100, 20):
       try:
         curl = url % (cuisine, i)
         conn = urlopen(curl)
@@ -33,71 +35,98 @@ def retrieve_cuisine():
       links = [anchor['href'] for anchor in anchors 
               if anchor.text == 'Get Recipe']
       alllinks.extend(links)
-  print alllinks
-  print len(alllinks)
-  return alllinks
+    cuisine_dict[cuisine] = alllinks
+  print cuisine_dict
+  print len(cuisine_dict)
+  return cuisine_dict
 
-def ingredients(links):
+def ingredients(cuisine_links):
   # make this a constant; base
   url = URL_BASE + '%s'
   total_ingredients = []
 
-  f2 = open('tmp2.txt', 'w')
-  for link in links:
-    try:
-      curl = url % link
-      conn = urlopen(curl)
-      data = conn.read()
-      conn.close()
-      soup = BeautifulSoup.BeautifulSoup(data)
-    except URLError as e:
-      print 'Error retrieving %s -> %s' % (curl, str(e))
-      continue
+  dbconn, cursor = db_instance('/home/kip/foodnetwork/db')
+  createdb(cursor)
 
-    links = soup.findAll('a',attrs = {'class': 'crosslink'})
-    #links = soup.findAll('a')
+  for cuisine, links in cuisine_links.iteritems():
     for link in links:
-    	atext = link.text
-    	link.replaceWith(atext)
+      try:
+        curl = url % link
+        conn = urlopen(curl)
+        data = conn.read()
+        conn.close()
+        soup = BeautifulSoup.BeautifulSoup(data)
+      except URLError as e:
+        print 'Error retrieving %s -> %s' % (curl, str(e))
+        continue
+
+      links = soup.findAll('a',attrs = {'class': 'crosslink'})
+      #links = soup.findAll('a')
+      for link in links:
+        atext = link.text
+        link.replaceWith(atext)
+
+      ingredients = soup.findAll('li', attrs = {'class': 'ingredient'})
+      total_ingredients.extend([strip_tags(str(ingred))
+                                for ingred in ingredients])
+    freq_analysis(dbconn, cursor, cuisine, total_ingredients)
 
 
-    ingredients = soup.findAll('li',attrs = {'class': 'ingredient'})
-    total_ingredients.extend([str(ingred).lower().replace('<li class="ingredient">','').replace('</li>','').replace('\r','')
-                              for ingred in ingredients])
-
-    print total_ingredients
-  # store in sqlite
-  #print total_ingredients
-  f2.close()
-  f = open('ingredients_indian.pkl', 'wb')
+  dbconn.commit()
+  dbconn.close()
+  f = open('ingredients_mexican.pkl', 'wb')
   pickle.dump(total_ingredients, f)
   f.close()
 
-def load():
-  f = open('ingredients_indian.pkl', 'rb')
-  ingredients_list = pickle.load(f)
-  f.close()
+def strip_tags(s):
+  return s.lower().replace('<li class="ingredient">','').replace('</li>','').replace('\r','')
+
+def remove_punc(s):
+  return s.replace(',', '').replace('(', '').replace(')','').replace('.','').replace("'","").replace(':','').strip()
+
+def freq_analysis(conn, cursor, cuisine, ingredients_list):
+  #f = open('ingredients_mexican.pkl', 'rb')
+  #ingredients_list = pickle.load(f)
+  #f.close()
+
   stopwords = [word[:-1] for word in open('stopwords.txt', 'r')]
   pstemmer = stemmer.PorterStemmer()
 
+
   freq = defaultdict(lambda : 1)
-  mapping = {}
+  mapping = defaultdict(list)
+
   for ingredients in ingredients_list:
     for ingredient in ingredients.split():
-      ingredient = ingredient.replace(',', '').replace('(', '').replace(')','').replace('.','').replace("'","").strip()
+      ingredient = remove_punc(ingredient)
       if ingredient not in stopwords:
         ingredient_stem = pstemmer.stem(ingredient, 0, len(ingredient)-1)
         #freq[ingredient] += 1
         freq[ingredient_stem] += 1
-        #mapping[ingredient_stem] = ingredient
+        mapping[ingredient_stem].append(ingredient)
 
   #for x,y in sorted(freq.iteritems(), key=lambda x: x[1], reverse=True):
-  for x,y in sorted(freq.iteritems(), key=operator.itemgetter(1), reverse=True):
-    print x,y
+  for ingred, freq in sorted(freq.iteritems(), key=operator.itemgetter(1), reverse=True):
+    print ingred, freq
+    nonstemmed_ingred = mapping[ingred]
+    dbingred = min(nonstemmed_ingred, key=lambda candidate: len(candidate))
+    insert_ingred(cursor, cuisine, dbingred, freq)
+    
 
+
+def db_instance(path):
+  conn = sqlite3.connect(path)
+  return (conn, conn.cursor())
+
+def createdb(c):
+  c.execute("""drop table if exists frequency""")
+  c.execute("""create table frequency (cuisine_type text, ingredient text, freq real)""")
+
+def insert_ingred(c, cuisine, ing, freq):
+  c.execute("""insert into frequency values ('%s', '%s', '%s')""" % (cuisine, ing, freq))
+  
 links = retrieve_cuisine()
 ingredients(links)
-#load()
 
 
 
